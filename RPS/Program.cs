6aa1320870c6871f952f7cb152386a58c28a8afc
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-22 15:01:35 +0100

    Use array to hold allowed signs

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -1,15 +1,14 @@
-﻿const string firstAllowedSign = "fire";
-const string secondAllowedSign = "water";
-const string thirdAllowedSign = "grass";
+﻿//                         0       1         2
+string[] allowedSigns = ["fire", "water", "grass"];
 
 Console.WriteLine("Zagrajmy w rock paper scissors");
-Console.WriteLine($"Graczu 1, podaj znak ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
+Console.WriteLine($"Graczu 1, podaj znak ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
 string? firstSign = Console.ReadLine();
-
-while (firstSign != firstAllowedSign && firstSign != secondAllowedSign && firstSign != thirdAllowedSign)
+// dopóki warunek w nawiasie okrągłymjest spełniony, wykonaj to, co jest w nawiasie klamrowym poniżej
+while (firstSign != allowedSigns[0] && firstSign != allowedSigns[1] && firstSign != allowedSigns[2])
 {
     Console.WriteLine("Niepoprawny znak");
-    Console.WriteLine($"Graczu 1, podaj POPRAWNY znak ({firstAllowedSign}/{secondAllowedSign}/{thirdAllowedSign})");
+    Console.WriteLine($"Graczu 1, podaj POPRAWNY znak ({allowedSigns[0]}/{allowedSigns[1]}/{allowedSigns[2]})");
     firstSign = Console.ReadLine();
 }
 
@@ -21,9 +20,9 @@ if (firstSign == secondSign)
     Console.WriteLine("Remis");
     Console.WriteLine("Ależ jesteście zgodni");
 }
-else if ((firstSign == firstAllowedSign && secondSign == thirdAllowedSign) 
-        || (firstSign == secondAllowedSign && secondSign == firstAllowedSign) 
-        || (firstSign == thirdAllowedSign && secondSign == secondAllowedSign))
+else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2]) 
+        || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0]) 
+        || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
 {
     Console.WriteLine("G1 MISZCZ!");
 } 
