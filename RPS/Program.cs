0679a7799d4a05604c0975cb66196ae9b878555b
Konrad Gadzina <konrad@enklawa.edu.pl>
2024-12-22 17:23:35 +0100

    Use break to stop the game

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -2,33 +2,42 @@
 string[] allowedSigns = ["fire", "water", "grass"];
 int firstPlayerPoints = 0;
 int secondPlayerPoints = 0;
+int expectedPoints = 3;
 
 Console.WriteLine($"Zagrajmy w {string.Join(" ", allowedSigns)}");
 
-string firstSign = GetSign(1);
-string secondSign = GetSign(2);
+while (true)
+{
+    string firstSign = GetSign(1);
+    string secondSign = GetSign(2);
 
-if (firstSign == secondSign)
-{
-    Console.WriteLine("Remis");
-    Console.WriteLine("Ależ jesteście zgodni");
-}
-else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2]) 
-        || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0]) 
-        || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
-{
-    Console.WriteLine("G1 MISZCZ!");
-    // firstPlayerPoints = firstPlayerPoints + 1;
-    firstPlayerPoints += 1;
-    // firstPlayerPoints++;
-} 
-else
-{
-    Console.WriteLine("G2 MISZCZ!");
-    secondPlayerPoints += 1;
+    if (firstSign == secondSign)
+    {
+        Console.WriteLine("Remis");
+        Console.WriteLine("Ależ jesteście zgodni");
+    }
+    else if ((firstSign == allowedSigns[0] && secondSign == allowedSigns[2])
+            || (firstSign == allowedSigns[1] && secondSign == allowedSigns[0])
+            || (firstSign == allowedSigns[2] && secondSign == allowedSigns[1]))
+    {
+        Console.WriteLine("G1 MISZCZ!");
+        // firstPlayerPoints = firstPlayerPoints + 1;
+        firstPlayerPoints += 1;
+        // firstPlayerPoints++;
+    }
+    else
+    {
+        Console.WriteLine("G2 MISZCZ!");
+        secondPlayerPoints += 1;
+    }
+    Console.WriteLine($"G1: {firstPlayerPoints} - G2: {secondPlayerPoints}");
+
+    if (firstPlayerPoints == expectedPoints || secondPlayerPoints == expectedPoints)
+    {
+        break;
+    }
 }
 
-Console.WriteLine($"G1: {firstPlayerPoints} - G2: {secondPlayerPoints}");
 Console.WriteLine("GG");
 
 // typ_zwracanych_danych NazwaMetody(typ_danej danaPierwsza, typ_danej danaDruga)
